9e41e9018b312a968d73a9d53a8d5d15aaf657e3
Sophia Gomes <sophiagomes.tds2.senai@gmail.com>
2024-10-10 16:37:45 -0300

    criação do cadastrar novo candidato

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -6,28 +6,28 @@ let suspeitos = [
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Mariana Cardoso",
-    profissão: "Dev",
+    profissao: "Dev",
     aposta: "sim",
     suspeita: "alto"
   },
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Marcelo vei",
-    profissão: "Docente",
+    profissao: "Docente",
     aposta: "não",
     suspeita: "baixo"
   },
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Felipe Dev",
-    profissão: "Cozinheiro",
+    profissao: "Cozinheiro",
     aposta: "sim",
     suspeita: "médio"
   },
   {
     id: Math.floor(Math.random() * 1000000),
     nome: "Marcelo Jordão",
-    profissão: "Protético",
+    profissao: "Protético",
     aposta: "não",
     suspeita: "baixo",
   },
@@ -38,4 +38,40 @@ suspeitosRoutes.get("/", (req, res) => {
     return res.status(200).json(suspeitos);
   });
 
+  // Rota para cadastrar um novo suspeito
+suspeitosRoutes.post("/", (req, res) => {
+  const { nome, profissao, aposta, suspeita } = req.body; 
+  
+   // Validação dos campos nome eprofissao
+   if (!nome || !profissao) {
+    return res.status(400).send({
+      message: "O nome ou a profissao não foi preenchido!.",
+    });
+  }
+
+    // Validação da suspeita
+    if (suspeita != "médio" && suspeita != "baixo" && suspeita != "alto") {
+      return res.status(400).send({
+        message:
+          "A suspeita deve ser classificado como baixo, médio ou alto!.!",
+      });
+    }
+
+   // Criação de um novo suspeito
+   const novoSuspeito = {
+    id: Math.floor(Math.random() * 1000000),
+    nome,
+   profissao,
+    aposta,
+    suspeita,
+  };
+
+  // Adiciona o novo suspeito ao array de suspeitos
+  suspeitos.push(novoSuspeito);
+
+  return res.status(201).json({
+    message: "Suspeito cadastrado com sucesso!",
+    novoSuspeito,
+  });
+});
   export default suspeitosRoutes;
